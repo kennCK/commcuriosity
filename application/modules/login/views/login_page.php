b39b3689bc43768f3ee8657a002019f7e7cf3ba1
kennCK <kennettecanales@gmail.com>
2016-07-08 19:32:51 +0800

    login

--- application/modules/login/views/login_page.php
+++ application/modules/login/views/login_page.php
@@ -1,13 +1,28 @@
 <link rel="stylesheet" href="<?php echo base_url().'assets/css/login/login_page.css';?>" />
 <div class="login-page-container">
 	<div id="login-p-header">
-		<h2>Login your account.</h2>
+		<h2 id="login-text-header">Login to your account.</h2>
 	</div>
 	<div id="login-p-body">
 		<div id="login-notif">
-			
+		</div>
+		<div id="login-content-holder">
+			<div id="login-field-icon"><img src="<?php echo base_url();?>assets/images/icons/ic_username.png" height="100%" width="100%"/></div>
+			<div id="login-textfield"><input type="text" id="username" class="textfield" placeholder="Username"/> </div>
+		</div>
+		
+		<div id="login-content-holder">
+			<div id="login-field-icon"><img src="<?php echo base_url();?>assets/images/icons/ic_password.png" height="100%" width="100%"/></div>
+			<div id="login-textfield"><input type="password" id="password" class="textfield" placeholder="********"/> </div>
 		</div>
 	</div>
 	<div id="login-p-footer">
+		<div id="login-button">
+			<input type="button" class="button" id="login-account" value="Submit"/>
+		</div>
+		<div id="forget-password">
+			<p>Forget your password?</p>
+		</div>
+
 	</div>
 </div>
